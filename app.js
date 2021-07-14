fd97818b258b9e02e176043062bf19a1af17edc5
piyushshende <48838255+piyushshende@users.noreply.github.com>
2021-07-14 15:20:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,21 +10,17 @@ window.addEventListener('load', function() {
         navigator.geolocation.getCurrentPosition(function(position) {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            console.log(lat, long);
             const lApi = `https://api.weatherapi.com/v1/current.json?key=4e0acdc7ad1e48bf978113712211307&q=${lat},${long}&aqi=no`;
-            // location = lApi.
             fetch(lApi)
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(data) {
                     country.innerText = data.location.name;
-                    console.log(data);
                     temp.innerText = data.current.temp_c;
                     description.innerText = data.current.condition.text;
                     image = document.createElement('img');
                     image.setAttribute('src', data.current.condition.icon);
-                    console.log(image);
                     icon.appendChild(image);
                 })
         });
