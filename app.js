2a7136a7cb2b4996fe2b10ed35a1b5e194012a50
piyushshende <48838255+piyushshende@users.noreply.github.com>
2021-07-15 11:50:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 window.addEventListener('load', function() {
     let long;
     let lat;
-    dcountry = document.querySelector('.desktop .country');
-    dtemp = document.querySelector('.desktop .temp');
-    ddescription = document.querySelector('.desktop .description');
-    dicon = document.querySelector('.desktop .icon');
-    mcountry = document.querySelector('.mobile .country');
-    mtemp = document.querySelector('.mobile .temp');
-    mdescription = document.querySelector('.mobile .description');
-    micon = document.querySelector('.mobile .icon');
-    let localizedName = '';
+    country = document.querySelectorAll('.country');
+    // console.log(country)
+    temp = document.querySelectorAll('.temp');
+    description = document.querySelectorAll('.description');
+    icon = document.querySelectorAll('.icon');
     if (this.navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            console.log(lat, long);
+            // console.log(lat, long);
             const lApi = `https://api.weatherapi.com/v1/current.json?key=4e0acdc7ad1e48bf978113712211307&q=${lat},${long}&aqi=no`;
             fetch(lApi)
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(data) {
-                    dcountry.innerText = data.location.name;
-                    mcountry.innerText = data.location.name;
-                    console.log(data);
-                    dtemp.innerText = data.current.temp_c;
-                    mtemp.innerText = data.current.temp_c;
-                    ddescription.innerText = data.current.condition.text;
-                    mdescription.innerText = data.current.condition.text;
+                    for (i = 0; i < country.length; i++) {
+                        country[i].innerText = data.location.name;
+                    }
+                    // console.log(data);
+                    for (i = 0; i < temp.length; i++) {
+                        temp[i].innerText = data.current.temp_c;
+                    }
+                    for (i = 0; i < description.length; i++) {
+                        description[i].innerText = data.current.condition.text;
+                    }
                     image = document.createElement('img');
                     image.setAttribute('src', data.current.condition.icon);
-                    console.log(image);
-                    dicon.appendChild(image);
-                    micon.appendChild(image);
+                    // console.log(image);
+                    for (i = 0; i < icon.length; i++) {
+                        icon[i].appendChild(image);
+                    }
                 });
         });
     }
